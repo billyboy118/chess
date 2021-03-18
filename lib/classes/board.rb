0ed37f57fcf2374fbc47d655362065ce336f6889
billyboy118 <bill_matley@hotmail.com>
2021-03-18 18:34:25 +0000

    Added board class with init and create_board method

--- lib/classes/board.rb
+++ lib/classes/board.rb
@@ -2,5 +2,17 @@
 
 # this class creates the board with the help of the Squares class
 class Board
+  def initialize
+    @board = []
+    create_board(board)
+  end
+
+  def create_board(board)
+    (0..7).each do |vertical|
+      (0..7).each do |horizontal|
+        board.push(Square.new([horizontal, vertical]))
+      end
+    end
+  end
 
 end
