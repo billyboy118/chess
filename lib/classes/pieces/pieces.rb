7428c77a239f1cbae1a4e000c9feaa736aab81e0
billyboy118 <bill_matley@hotmail.com>
2021-03-19 19:16:48 +0000

    Add all unicode pieces into ChessPieces module

--- lib/classes/pieces/pieces.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: false
-
-# this class creates the board with the help of the Squares class
-class Pieces
-
-end
--- lib/modules/chess_pieces.rb
+++ lib/modules/chess_pieces.rb
@@ -3,14 +3,12 @@
 # module for individual chess classes to assign unicode piece
 module ChessPieces
   def get_piece(piece_name, colour)
-    colour == 'Black' ? get_black(piece_name) : get_white(piece_name)
-  end
+    black_pieces = { 'Bishop' => "\u2657", 'King' => "\u2654", 'Knight' => "\u2658",
+                     'Pawn' => "\u2659", 'Queen' => "\u2655", 'Rook' => "\u2656" }
 
-  def get_black(piece_name)
-    case piece_name
-    when 'Bishop'
-      "\u2658"
-    end
+    white_pieces = { 'Bishop' => "\u265D", 'King' => "\u265A", 'Knight' => "\u265E",
+                     'Pawn' => "\u265F", 'Queen' => "\u265B", 'Rook' => "\u265C" }
 
+    colour == 'Black' ? black_pieces[piece_name] : white_pieces[piece_name]
   end
-end 
+end
