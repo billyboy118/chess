a2b9bf99b23a7f7833ff1a06a6ced76ec5ffb1f2
billyboy118 <bill_matley@hotmail.com>
2021-03-30 20:01:03 +0100

    Edited existing can_pawn_take method and created methods to stop the pawn taking when moving forward

--- lib/classes/pieces/pawn.rb
+++ lib/classes/pieces/pawn.rb
@@ -10,16 +10,58 @@ class Pawn < Pieces
   end
 
   def allocate_moves
-    return @moves = [[0, 2], [0, 1], [1, 1], [-1, 1]] if colour == 'White'
+    return @moves = [[0, 1], [-1, 1], [1, 1], [0, 2]] if colour == 'White'
 
-    @moves = [[0, -2], [0, -1], [1, -1], [-1, 1]]
+    @moves = [[0, -1], [-1, -1], [1, -1], [0, -2]]
   end
 
   def calculate_pawn
     allocate_moves
-    moves.shift if has_piece_moved == 'Yes'
+    moves.pop if has_piece_moved == 'Yes'
     can_pawn_take
     calculate_positions
     return true if potential_moves.include?(move_to.position)
+  end
+
+  def can_pawn_take
+    left = calculate_left
+    right = calculate_right
+    front = calculate_front
+    double_front = calculate_double_front
+
+    moves.delete_at(2) if look_potential_moves(right)
+    moves.delete_at(1) if look_potential_moves(left)
+    moves.delete_at(0) unless look_potential_moves(front)
+    moves.pop unless look_potential_moves(double_front)
+  end
+
+  def correct_range(position)
+    position.all? { |num| num >= 0 && num < 8 }
+  end
+
+  def look_potential_moves(position)
+    position.nil? || position.current_piece == ' '
+  end
+
+  def calculate_left
+    left = [move_from[0] + moves[1][0], move_from[1] + moves[1][1]]
+    Generic.find_square(left, board) if correct_range(left)
+  end
+
+  def calculate_right
+    right = [move_from[0] + moves[2][0], move_from[1] + moves[2][1]]
+    Generic.find_square(right, board) if correct_range(right)
+  end
+
+  def calculate_front
+    front = [move_from[0] + moves[0][0], move_from[1] + moves[0][1]]
+    Generic.find_square(front, board) if correct_range(front)
+  end
+
+  def calculate_double_front
+    return if has_piece_moved == 'Yes'
+
+    double_front = [move_from[0] + moves[3][0], move_from[1] + moves[3][1]]
+    Generic.find_square(double_front, board) if correct_range(double_front)
   end
 end
