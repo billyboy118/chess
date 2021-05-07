d4593b0c7476bd32e68776cff45e8c00a08b457d
billyboy118 <bill_matley@hotmail.com>
2021-05-07 19:45:40 +0100

    Promotion methods complete, all pawn methods now complete

--- lib/modules/promotion.rb
+++ lib/modules/promotion.rb
@@ -12,13 +12,32 @@ module Promotion
     select_new_piece
   end
 
+  # first stage
   def select_new_piece
     prompt = TTY::Prompt.new
     greeting = puts "\nWhat would you like to promote your piece to?"
     choices = %w[Queen Knight Bishop Rook]
     response = prompt.select(greeting, choices)
-
+    assign_piece(response)
   end
 
+  # second stage
+  def assign_piece(new_piece)
+    current_location = Generic.find_square_index(move_to.position, board)
+    board[current_location].current_piece = create_piece(new_piece)
+  end
 
+  # third stage
+  def create_piece(new_piece)
+    case new_piece
+    when 'Queen'
+      Queen.new(player_name, colour)
+    when 'Knight'
+      Knight.new(player_name, colour)
+    when 'Bishop'
+      Bishop.new(player_name, colour)
+    when 'Rook'
+      Rook.new(player_name, colour)
+    end
+  end
 end
