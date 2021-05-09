e54eb921304d73d1d2acf37cdc7c9c9ebbae6d22
billyboy118 <bill_matley@hotmail.com>
2021-05-09 08:07:30 +0100

    Created the castling method which determines 1) the king has not moved 2) The move is 2 spaces to the left or 2 spaces to the right

--- lib/classes/pieces/king.rb
+++ lib/classes/pieces/king.rb
@@ -2,6 +2,7 @@
 
 # class which create king pieces to put on the board
 class King < Pieces
+  CASTLING_MOVES = [2, 58, 6, 62].freeze
   def initialize(name, colour)
     super(name, colour)
     @piece_name = 'King'
@@ -12,8 +13,22 @@ class King < Pieces
 
   def calculate_king
     calculate_positions
+    castling # return true if castlig == true # hide this so i can move the king anywhere
     return true if potential_moves.include?(move_to.position)
 
     false # to move king anywhere I can change this to a true
+  end
+
+  def castling
+    return if no_of_moves.positive?
+
+    # return if check == true
+    move_index = Generic.find_square_index(move_to.position, board)
+    case move_index
+    when 2, 58
+      castling move
+    when 6, 62
+      castling move
+    end
   end
 end
