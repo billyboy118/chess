1e346032a2b71c435599cff1d08db5b3cf7f2520
billyboy118 <bill_matley@hotmail.com>
2021-05-16 07:24:23 +0100

    Added the caal to th module to identify check

--- lib/modules/move_input.rb
+++ lib/modules/move_input.rb
@@ -18,6 +18,7 @@ module PlayerInput
   end
 
   def position
+    start_turn_identify_check
     puts "\nHit Enter for more options\n".red
     case game_phase
     when 1..2 then print 'Which piece would you like to select?: '
