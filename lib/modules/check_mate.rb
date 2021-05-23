3e2205d239f9f162bcbb3a6ac709d89a4497c16c
billyboy118 <bill_matley@hotmail.com>
2021-05-23 14:18:11 +0100

    Added checkmate module

---
+++ lib/modules/check_mate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: false
+
+# this module if to determine if the King has been put in check mate
+module CheckMate
+  def start_check_mate
+    # if player is in check then searchy for checkmate =, if they are in check mate end the game and give a load of options
+  end
+end
