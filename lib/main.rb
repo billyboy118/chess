93e21dcad53595dcea2c25db6bc0ac2cb83b7ade
billyboy118 <bill_matley@hotmail.com>
2021-06-25 18:38:32 +0100

    Tidy up code

--- lib/main.rb
+++ lib/main.rb
@@ -4,7 +4,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'modules')
 $LOAD_PATH << File.join(File.dirname(__FILE__), 'classes')
 $LOAD_PATH << File.join(File.dirname(__FILE__), './classes/pieces')
 require 'yaml'
-# require 'json'
+
 require 'show_board'
 require 'generic'
 require 'start_text'
